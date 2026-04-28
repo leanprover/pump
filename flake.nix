5daa282f88df132785a27f928e72ceaf189e35f6
Joscha <joscha@plugh.de>
2026-04-28 18:55:03 +0200

    Set up flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+
+    impeller.url = "github:leanprover/impeller";
+    impeller.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    let
+      lib = inputs.nixpkgs.lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          craneLib = inputs.crane.mkLib pkgs;
+          impeller = inputs.impeller.packages.${system}.default;
+        in
+        {
+          default = craneLib.buildPackage {
+            src = ./.;
+            buildInputs = [ impeller ];
+          };
+        }
+      );
+    };
+}
