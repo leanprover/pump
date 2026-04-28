a9f3ad8b41f7a5b842802a639ce7658e7c631a9e
Joscha <joscha@plugh.de>
2026-04-28 19:13:59 +0200

    Fix JSON version tagging

--- src/data/job.rs
+++ src/data/job.rs
@@ -20,7 +20,7 @@ pub struct JobQueryV0 {
 }
 
 #[derive(Clone, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "snake_case")]
+#[serde(tag = "version", rename_all = "snake_case")]
 pub enum JobQuery {
     V0(JobQueryV0),
 }
