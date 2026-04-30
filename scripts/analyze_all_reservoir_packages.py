d7f63be30fc8c0e6f6edc6b3b74e6e817e42e3c1
Joscha <joscha@plugh.de>
2026-04-30 17:28:10 +0200

    Build latest versions of packages in test script

--- scripts/analyze_all_reservoir_packages.py
+++ scripts/analyze_all_reservoir_packages.py
@@ -1,13 +1,13 @@
 import json
 import time
 from argparse import ArgumentParser
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 import requests
 from rich import print
 from rich.progress import BarColumn, Progress
+from semver import Version
 
 RESERVOIR_MANIFEST_URL = "https://reservoir.lean-lang.org/index/manifest.json"
 
@@ -43,119 +43,125 @@ def fetch_manifest(manifest_file: Path) -> Any:
     return manifest
 
 
-@dataclass
-class Source:
-    owner: str
-    repo: str
-
-
-@dataclass
-class Version:
-    source: Source
-    tag: str
-    sha: str
-
-
-def get_sources(manifest: Any, sources: dict[str, Source]):
+def get_sources_from_manifest(manifest: Any) -> list[tuple[str, str]]:
+    result = []
     for package in manifest["packages"]:
         source = package["sources"][0]
         full_name = source["fullName"]
         owner, repo = full_name.split("/")
-        sources[full_name] = Source(owner, repo)
+        result.append((owner, repo))
+    return result
 
 
-def build_job_analyze_global(source: Source) -> dict[str, Any]:
-    return {
-        "version": "v0",
-        "type": "analyze_global",
-        "input": {
-            "version": "v0",
-            "source": {
-                "type": "github",
-                "owner": source.owner,
-                "repo": source.repo,
+class Collector:
+    def __init__(self):
+        self.queries: dict[str, Any] = {}
+        self.results: dict[str, Any] = {}
+
+    def query(self, key: str, job: Any) -> Any | None:
+        self.queries[key] = job
+        return self.results.get(key)
+
+    def _open_queries(self, limit: int) -> dict[str, Any]:
+        result = {}
+        for key, job in sorted(self.queries.items()):
+            if len(result) >= limit:
+                break
+            if key in self.results:
+                continue
+            result[key] = job
+        return result
+
+    def _update_results(self, new_results: dict[str, Any]) -> None:
+        self.results.update(new_results)
+
+    def update(
+        self, pump_url: str, limit: int
+    ) -> tuple[dict[str, Any], dict[str, Any]]:
+        jobs = self._open_queries(limit)
+
+        res = requests.post(f"{pump_url}/query", json={"jobs": jobs})
+        res.raise_for_status()
+        reply = res.json()
+        pending = reply["pending"]
+        completed = reply["completed"]
+
+        self._update_results(completed)
+        return pending, completed
+
+    def status_prefixed(self, prefix: str) -> tuple[int, int]:
+        queries = sum(1 for k in self.queries if k.startswith(prefix))
+        results = sum(1 for k in self.results if k.startswith(prefix))
+        return queries, results
+
+
+def parse_version(version: str) -> Version | None:
+    try:
+        return Version.parse(version.removeprefix("v"))
+    except ValueError:
+        pass
+
+
+def sorted_versions(versions: list[str]) -> list[str]:
+    pv = [(p, v) for v in versions if (p := parse_version(v))]
+    pv.sort()
+    return [v for _, v in pv]
+
+
+def derive_jobs(collector: Collector, sources: list[tuple[str, str]]) -> None:
+    for owner, repo in sorted(sources):
+        result_global = collector.query(
+            f"a/{owner}/{repo}",
+            {
+                "version": "v0",
+                "type": "analyze_global",
+                "input": {
+                    "version": "v0",
+                    "source": {"type": "github", "owner": owner, "repo": repo},
+                },
             },
-        },
-    }
-
-
-def build_job_analyze_version(version: Version) -> dict[str, Any]:
-    return {
-        "version": "v0",
-        "type": "analyze_version",
-        "input": {
-            "version": "v0",
-            "source": {
-                "type": "github",
-                "owner": version.source.owner,
-                "repo": version.source.repo,
-            },
-            "sha": version.sha,
-        },
-    }
-
-
-def process_global_info(
-    sources: dict[str, Source], versions: dict[str, Version], key: str, info: Any
-) -> None:
-    output = info.get("output")
-    if output is None:
-        return
-
-    source = sources[key]
-
-    version_tags = (output.get("lake") or {}).get("version_tags")
-    if version_tags is None:
-        version_tags = output["git"]["version_tags"]
-
-    for tag in version_tags:
-        sha = output["git"]["tag_shas"][tag]
-        # Flipped intentionally so queued tasks are sorted into a pseudorandom
-        # but deterministic order.
-        vkey = f"{sha}/{key}"
-        versions[vkey] = Version(source, tag, sha)
-
-
-def query(
-    args: Args,
-    sources: dict[str, Source],
-    versions: dict[str, Version],
-    info_global: dict[str, Any],
-    info_version: dict[str, Any],
-) -> tuple[dict[str, Any], dict[str, Any]]:
-    jobs = {}
-
-    for key, source in sorted(sources.items()):
-        if len(jobs) >= args.batch_size:
-            break
-        if key in info_global:
+        )
+        if result_global is None:
             continue
-        jobs[key] = build_job_analyze_global(source)
-
-    for key, version in sorted(versions.items()):
-        if len(jobs) >= args.batch_size:
-            break
-        if key in info_version:
+        output_global = result_global.get("output")
+        if output_global is None:
             continue
-        jobs[key] = build_job_analyze_version(version)
 
-    res = requests.post(f"{args.pump_url}/query", json={"jobs": jobs})
-    res.raise_for_status()
-    reply = res.json()
+        version_tags = (output_global.get("lake") or {}).get("version_tags")
+        if version_tags is None:
+            version_tags = output_global["git"]["version_tags"]
 
-    pending = reply["pending"]
-    completed = reply["completed"]
-    c_global = {k: v for k, v in completed.items() if v["type"] == "analyze_global"}
-    c_version = {k: v for k, v in completed.items() if v["type"] == "analyze_version"}
+        for tag in version_tags:
+            sha = output_global["git"]["tag_shas"][tag]
+            collector.query(
+                f"b/{sha}/{owner}/{repo}",
+                {
+                    "version": "v0",
+                    "type": "analyze_version",
+                    "input": {
+                        "version": "v0",
+                        "source": {"type": "github", "owner": owner, "repo": repo},
+                        "sha": sha,
+                    },
+                },
+            )
 
-    for key, result in c_global.items():
-        info_global[key] = result
-        process_global_info(sources, versions, key, result)
-
-    for key, result in c_version.items():
-        info_version[key] = result
-
-    return pending, completed
+        sorted_version_tags = sorted_versions(version_tags)
+        if sorted_version_tags:
+            tag = sorted_version_tags[-1]
+            sha = output_global["git"]["tag_shas"][tag]
+            collector.query(
+                f"c/{sha}/{owner}/{repo}",
+                {
+                    "version": "v0",
+                    "type": "build_version",
+                    "input": {
+                        "version": "v0",
+                        "source": {"type": "github", "owner": owner, "repo": repo},
+                        "sha": sha,
+                    },
+                },
+            )
 
 
 def main() -> None:
@@ -166,14 +172,12 @@ def main() -> None:
     parser.add_argument("-i", "--poll-interval", type=int, default=2)
     args = parser.parse_args(namespace=Args())
 
-    sources: dict[str, Source] = {}
-    versions: dict[str, Version] = {}
-    info_global: dict[str, Any] = {}
-    info_version: dict[str, Any] = {}
-
     manifest = fetch_manifest(args.manifest_file)
-    get_sources(manifest, sources)
+    sources = get_sources_from_manifest(manifest)
     print(f"Found {len(sources)} sources in manifest")
+
+    collector = Collector()
+    derive_jobs(collector, sources)
 
     with Progress(
         "{task.description}",
@@ -182,19 +186,25 @@ def main() -> None:
     ) as p:
         tp = p.add_task("pending", total=args.batch_size)
         tr = p.add_task("running", total=args.batch_size)
-        tg = p.add_task("analyze-global", total=len(sources))
-        tv = p.add_task("analyze-version", total=0)
-        while len(info_global) < len(sources) or len(info_version) < len(versions):
-            pending, completed = query(
-                args, sources, versions, info_global, info_version
-            )
+        tg = p.add_task("global", total=0)
+        tv = p.add_task("version", total=0)
+        tb = p.add_task("build", total=0)
 
-            running = {k: v for k, v in pending.items() if v["started"]}
+        while len(collector.results) < len(collector.queries):
+            pending, completed = collector.update(args.pump_url, args.batch_size)
+            derive_jobs(collector, sources)
 
-            p.update(tp, completed=len(pending))
-            p.update(tr, completed=len(running))
-            p.update(tg, completed=len(info_global), total=len(sources))
-            p.update(tv, completed=len(info_version), total=len(versions))
+            n_pending = len(pending)
+            n_running = sum(1 for v in pending.values() if v["started"])
+            tg_queries, tg_results = collector.status_prefixed("a/")
+            tv_queries, tv_results = collector.status_prefixed("b/")
+            tb_queries, tb_results = collector.status_prefixed("c/")
+
+            p.update(tp, completed=n_pending)
+            p.update(tr, completed=n_running)
+            p.update(tg, total=tg_queries, completed=tg_results)
+            p.update(tv, total=tv_queries, completed=tv_results)
+            p.update(tb, total=tb_queries, completed=tb_results)
             p.refresh()
 
             if not completed:
